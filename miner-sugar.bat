c9664256ff4e79cc6fc7cd8fea3e13b567899bae
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2021-02-25 17:49:07 +0900

    Add files via upload
    rt

---
+++ miner-sugar.bat
@@ -0,0 +1,10 @@
+rem
+rem Choose nearest stratum:
+rem       stratum-ru.rplant.xyz   /Moscow/
+rem       stratum-eu.rplant.xyz   /London/
+rem       stratum-asia.rplant.xyz /Singapore/
+rem       stratum-na.rplant.xyz   /Toronto/
+rem
+:start
+cpuminer-sse2.exe -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qf4hwy6qyl6adlv3xrjhpy4ymmm07m3k2yv6jtw.2core -t2
+goto start
